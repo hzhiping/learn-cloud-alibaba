f3f3bd73bbe7ab8bbbf0a1c5159fc69c4b67ce94
hzhiping <23955637@qq.com>
2023-02-25 22:50:42 +0800

    feature:添加微服务ConfigClient客户端代码

---
+++ cloud-config-client-3355/src/main/java/com/hzhiping/ConfigClientApplication3355.java
@@ -0,0 +1,17 @@
+package com.hzhiping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author hzhiping
+ * @date 2023/02/25
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientApplication3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication3355.class, args);
+    }
+}
---
+++ cloud-config-client-3355/src/main/java/com/hzhiping/controller/ConfigClientController.java
@@ -0,0 +1,22 @@
+package com.hzhiping.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author hzhiping
+ * @date 2023/02/25
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+}
