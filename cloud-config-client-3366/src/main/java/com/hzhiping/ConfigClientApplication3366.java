230a84042c2ee4e60c4d4f378783a455e8f0890d
hzhiping <23955637@qq.com>
2023-02-28 19:03:52 +0800

    feature:添加Bus消息总线

---
+++ cloud-config-client-3366/src/main/java/com/hzhiping/ConfigClientApplication3366.java
@@ -0,0 +1,17 @@
+package com.hzhiping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author hzhiping
+ * @date 2023/02/26
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class ConfigClientApplication3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication3366.class, args);
+    }
+}
---
+++ cloud-config-client-3366/src/main/java/com/hzhiping/controller/ConfigClientController.java
@@ -0,0 +1,24 @@
+package com.hzhiping.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author hzhiping
+ * @date 2023/02/25
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+    @Value("${server.port}")
+    private String serverPort;
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo() {
+        return "serverPort：" + serverPort + "\t\n\n configInfo：" + configInfo;
+    }
+}
