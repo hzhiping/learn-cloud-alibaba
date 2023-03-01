b15d1e15ad74ebc238a11d96f58d636097134d29
hzhiping <23955637@qq.com>
2023-03-01 21:42:26 +0800

    feature:介绍分组和持久化消费问题

---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/hzhiping/StreamMqApplication8003.java
@@ -0,0 +1,15 @@
+package com.hzhiping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hzhiping
+ * @date 2023/03/01
+ */
+@SpringBootApplication
+public class StreamMqApplication8003 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMqApplication8003.class, args);
+    }
+}
