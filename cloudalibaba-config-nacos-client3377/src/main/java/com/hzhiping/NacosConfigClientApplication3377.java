72c39dbfbabbb50f27e291305ce7b1d79c99cdb5
hzhiping <23955637@qq.com>
2023-03-04 20:10:07 +0800

    feature:添加Nacos作为配置中心代码

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/hzhiping/NacosConfigClientApplication3377.java
@@ -0,0 +1,17 @@
+package com.hzhiping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author hzhiping
+ * @date 2023/03/04
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class NacosConfigClientApplication3377 {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientApplication3377.class, args);
+    }
+}
